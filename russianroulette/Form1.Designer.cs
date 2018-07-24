233388fdc5751f4db67209140aa2de6b4c14dfee
Bhupinder Singh <BS029@atcltd.atc.org.nz>
2018-07-25 11:16:09 +1200

    Ankusdeep Singh Assignment

--- russianroulette/Form1.Designer.cs
+++ russianroulette/Form1.Designer.cs
@@ -28,18 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(128, 33);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(153, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(352, 33);
@@ -48,10 +59,10 @@
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(533, 343);
+            this.button1.Font = new System.Drawing.Font("Rockwell", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(592, 270);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.Size = new System.Drawing.Size(110, 37);
             this.button1.TabIndex = 1;
             this.button1.Text = "Close";
             this.button1.UseVisualStyleBackColor = true;
@@ -59,8 +70,8 @@
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(519, 122);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(592, 215);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(110, 37);
             this.button2.TabIndex = 2;
@@ -70,25 +81,14 @@
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button3.Location = new System.Drawing.Point(12, 138);
+            this.button3.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 215);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(90, 33);
             this.button3.TabIndex = 3;
             this.button3.Text = "Fire";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.button4.Location = new System.Drawing.Point(12, 188);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(90, 33);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Fire Away (2)";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // label2
             // 
@@ -101,25 +101,121 @@
             this.label2.Text = "label2";
             this.label2.Visible = false;
             // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(12, 272);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 33);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Fire Away (2)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(532, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(88, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Shots Remaining:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(205, 348);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 26);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "6";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(296, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 26);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Probability of getting Killed:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bradley Hand ITC", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(587, 348);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 26);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "5/6";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(301, 215);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(131, 109);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Chiller", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(154, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(351, 56);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Luck Won\'t Save You!";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 33F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(641, 392);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(773, 392);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(8, 8, 8, 8);
+            this.Margin = new System.Windows.Forms.Padding(8);
             this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Russian Roulette with Gun";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,8 +227,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- russianroulette/game.cs
+++ russianroulette/game.cs
@@ -9,66 +9,73 @@ namespace russianroulette
 {
     class Game
     {
-        public int[] gunBarrel = { 0, 0, 0, 0, 0, 0 };
-        int chances = 2;
-        public void start()
+                                                                    //initialising array as gun chamber
+        public int[] gunBarrel = { 0, 0, 0, 0, 0, 0 };  if          //empty bullet space is 0 and with bullet it is 1
+        public int chances = 2;                                     //Chances for the player
+        public void start()                                         //Game Start
         {
-            Random rand = new Random();
+            Random rand = new Random();                             //Using random function to place bullet in the gun
             int val = rand.Next(0, 5);
             gunBarrel[val] = 1;
-            Console.WriteLine("" + val);
-            loadBarrel();
+            loadBarrel();                                           //to make chamber spin sound
         }
 
-        public void loadBarrel()
+        public void loadBarrel()                                    //making chamber spin sound
         {
             SoundPlayer player = new SoundPlayer("loading_and_spinning.wav");
             player.Play();
         }
 
-        public string fire()
+        public string fire()                                        //function for firing---- returning string to identify what happened on gun fire
         {
-            if (gunBarrel[0] == 1)
+            if (gunBarrel[0] == 1)                                  //checking if bullets in place
             {
-                SoundPlayer player = new SoundPlayer("gun_shot.wav");
+                SoundPlayer player = new SoundPlayer("gun_shot.wav");       //making gun shot fire sound 
                 player.Play();
-                return "killed";
+                return "killed";                                            //returning - the player is killed.
             }
-            else
+            else                                                    //otherwise
             {
-                SoundPlayer player = new SoundPlayer("gun_cock.wav");
+                SoundPlayer player = new SoundPlayer("gun_cock.wav");           //no bullet sound playing
                 player.Play();
-                gunBarrel = gunBarrel.Skip(1).ToArray();
+                gunBarrel = gunBarrel.Skip(1).ToArray();                        //removing one elemnt from array to make the bullet shift forward
                 return "safe";
             }
         }
 
-        public string fireAway()
+        public string fireAway()                                    //Firing away
         {
-            chances--;
-            if (chances == 0)
-            {
-                if (gunBarrel[0] == 1)
+            chances--;                                              //reducing the chances player has
+            if (chances == 0)                                           
+            {   
+                if (gunBarrel[0] == 1)                              //if chances becomes 0 and the bullet is in position - the player has survived
                 {
-                    return "bullet gone";
+                    return "bullet gone";                           //returning he won
                 }
                 else
                 {
-                    gunBarrel = gunBarrel.Skip(1).ToArray();
-                    return "nothing";
+                    gunBarrel = gunBarrel.Skip(1).ToArray();        //removing element from array to show no bullet was there to fire and no chances are there. so player dies.
+                    return "game end";
                 }
             }
             else
             {
-                if (gunBarrel[0] == 1)
+                if (gunBarrel[0] == 1)                              //on first chance if the bullet is in position - the player has survived
                 {
-                    return "You win";
+                    return "You win";                               //returning - the player has won
                 }
-                else
+                else                                                //otherwise
                 {
-                    return "nothing";
+                    return "nothing";                               //the firing away was wasted.
                 }
             }
+        }
+
+        public void end()                                           //when game ends
+        {
+            gunBarrel = new int[] { 0, 0, 0, 0, 0, 0 };             //setting up for next play.
+            chances = 2;                                            //cahnces are 2
+
         }
     }
 }
